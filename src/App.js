1f73f7c862f44362d2b1c4a4b894b42c27e7bba2
perttuvirkki <virkkiperttu@gmail.com>
2023-11-09 13:44:04 +0200

    axios apicalls

--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [ticketId, setTicketId] = useState("");
+  const [ticketData, setTicketData] = useState(null);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(`/api/tickets/${ticketId}`);
+      setTicketData(response.data);
+      console.log("ticketData: ", ticketData);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+      setTicketData(null);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" value={ticketId} onChange={(e) => setTicketId(e.target.value)} placeholder="Enter Ticket ID" />
+        <button onClick={handleSearch}>Search</button>
+        {ticketData && (
+          <div className="ticket-result">
+            <div>
+              <strong>Ticket ID:</strong> {ticketData.ticketId}
+            </div>
+            <div>
+              <strong>Event Name:</strong> {ticketData.event.eventName}
+            </div>
+            <div>
+              <strong>Description:</strong> {ticketData.event.description}
+            </div>
+            <div>
+              <strong>Event Date:</strong> {ticketData.event.eventDate}
+            </div>
+            <div>
+              <strong>Event Time:</strong> {ticketData.event.eventTime}
+            </div>
+            <div>
+              <strong>Venue:</strong> {ticketData.event.venue.place}
+            </div>
+            <div>
+              <strong>Address:</strong> {ticketData.event.venue.streetAddress}
+            </div>
+            <div>
+              <strong>Postal Code:</strong> {ticketData.event.venue.postalcode.postalcode}
+            </div>
+            <div>
+              <strong>Post Office:</strong> {ticketData.event.venue.postalcode.postOffice}
+            </div>
+            <div>
+              <strong>Ticket Type:</strong> {ticketData.ticketType.ticketType}
+            </div>
+            <div>
+              <strong>Price:</strong> {ticketData.ticketType.price}
+            </div>
+            <div>
+              <strong>Transaction Amount:</strong> {ticketData.transaction.amount}
+            </div>
+            <div>
+              <strong>Transaction Date:</strong> {ticketData.transaction.transactionDate}
+            </div>
+            <div>
+              <strong>Transaction Time:</strong> {ticketData.transaction.transactionTime}
+            </div>
+            <div>
+              <strong>Transaction Status:</strong> {ticketData.transaction.transactionOk ? "Successful" : "Failed"}
+            </div>
+            <div>
+              <strong>Checked:</strong> {ticketData.isChecked ? "Yes" : "No"}
+            </div>
+          </div>
+        )}
       </header>
     </div>
   );
