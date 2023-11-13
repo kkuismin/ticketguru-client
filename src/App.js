67872397ddd39fbf071be71ecaccc492a6d98ecb
perttuvirkki <virkkiperttu@gmail.com>
2023-11-13 18:16:50 +0200

    update button disabled if ticket is checked

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 //import "./App.css";
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Textfield from "@mui/material/TextField";
-import { Box } from '@mui/system';
-import AppBar from '@mui/material/AppBar';
-import ToolBar from '@mui/material/Toolbar';
+import { Box } from "@mui/system";
+import AppBar from "@mui/material/AppBar";
+import ToolBar from "@mui/material/Toolbar";
 
 function App() {
   const [ticketId, setTicketId] = useState("");
@@ -28,11 +28,11 @@ function App() {
   const handleChecking = async () => {
     try {
       const response = await axios.patch(`/api/tickets/${ticketId}/check`);
-      console.log("data", response.data)
+      console.log("data", response.data);
     } catch (error) {
       console.error("Error fetching data: ", error);
     }
-  }
+  };
 
   return (
     <Box>
@@ -42,33 +42,39 @@ function App() {
           <Typography variant="h5">TicketGuru</Typography>
         </ToolBar>
       </AppBar>
-      <Container maxWidth="sm" sx={{ marginTop: '100px', marginBottom: '50px', textAlign: 'center'}}>
+      <Container maxWidth="sm" sx={{ marginTop: "100px", marginBottom: "50px", textAlign: "center" }}>
         <Typography variant="h5">Search for a ticket</Typography>
         <Textfield sx={{ marginTop: 3 }} label="Enter Ticket ID" name={ticketId} onChange={(e) => setTicketId(e.target.value)} />
         <Box sx={{ marginTop: 3 }}>
-          <Button variant="contained" onClick={handleSearch}>Search</Button>
+          <Button variant="contained" onClick={handleSearch}>
+            Search
+          </Button>
         </Box>
         {ticketData && (
           <Box sx={{ marginTop: 3 }}>
-            <Typography variant="h5" sx={{ marginBottom: 3 }}>Ticket details</Typography>
-            <strong>Ticket ID:</strong> {ticketData.ticketId} <br/>
-            <strong>Event Name:</strong> {ticketData.event.eventName} <br/>
-            <strong>Description:</strong> {ticketData.event.description} <br/>
-            <strong>Event Date:</strong> {ticketData.event.eventDate} <br/>
-            <strong>Event Time:</strong> {ticketData.event.eventTime} <br/>
-            <strong>Venue:</strong> {ticketData.event.venue.place} <br/>
-            <strong>Address:</strong> {ticketData.event.venue.streetAddress} <br/>
-            <strong>Postal Code:</strong> {ticketData.event.venue.postalcode.postalcode} <br/>
-            <strong>Post Office:</strong> {ticketData.event.venue.postalcode.postOffice} <br/>
-            <strong>Ticket Type:</strong> {ticketData.ticketType.ticketType} <br/>
-            <strong>Price:</strong> {ticketData.ticketType.price} <br/>
-            <strong>Transaction Amount:</strong> {ticketData.transaction.amount} <br/>
-            <strong>Transaction Date:</strong> {ticketData.transaction.transactionDate} <br/>
-            <strong>Transaction Time:</strong> {ticketData.transaction.transactionTime} <br/>
-            <strong>Transaction Status:</strong> {ticketData.transaction.transactionOk ? "Successful" : "Failed"} <br/>
-            <strong>Checked:</strong> {ticketData.isChecked ? "Yes" : "No"} <br/>
+            <Typography variant="h5" sx={{ marginBottom: 3 }}>
+              Ticket details
+            </Typography>
+            <strong>Ticket ID:</strong> {ticketData.ticketId} <br />
+            <strong>Event Name:</strong> {ticketData.event.eventName} <br />
+            <strong>Description:</strong> {ticketData.event.description} <br />
+            <strong>Event Date:</strong> {ticketData.event.eventDate} <br />
+            <strong>Event Time:</strong> {ticketData.event.eventTime} <br />
+            <strong>Venue:</strong> {ticketData.event.venue.place} <br />
+            <strong>Address:</strong> {ticketData.event.venue.streetAddress} <br />
+            <strong>Postal Code:</strong> {ticketData.event.venue.postalcode.postalcode} <br />
+            <strong>Post Office:</strong> {ticketData.event.venue.postalcode.postOffice} <br />
+            <strong>Ticket Type:</strong> {ticketData.ticketType.ticketType} <br />
+            <strong>Price:</strong> {ticketData.ticketType.price} <br />
+            <strong>Transaction Amount:</strong> {ticketData.transaction.amount} <br />
+            <strong>Transaction Date:</strong> {ticketData.transaction.transactionDate} <br />
+            <strong>Transaction Time:</strong> {ticketData.transaction.transactionTime} <br />
+            <strong>Transaction Status:</strong> {ticketData.transaction.transactionOk ? "Successful" : "Failed"} <br />
+            <strong>Checked:</strong> {ticketData.isChecked ? "Yes" : "No"} <br />
             <Box sx={{ marginTop: 3 }}>
-              <Button variant="contained" onClick={handleChecking}>Mark ticket as used</Button>
+              <Button variant="contained" onClick={handleChecking} disabled={ticketData ? ticketData.isChecked : false}>
+                {ticketData && ticketData.isChecked ? "Ticket is used" : "Mark ticket as used"}
+              </Button>
             </Box>
           </Box>
         )}
